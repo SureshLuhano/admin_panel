9a02759c6e62bd3ddc5e2d92f85127ab8f726dad
SureshLuhano <skluhano65@gmail.com>
2024-02-06 18:06:56 +0500

    deploying to github

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { registerCharts } from './components/registerCharts';
+// import './App.css';
+import Navigation from './navigation/navigation';
+import "./assets/styles/global.scss";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import {store, persistor} from "./redux/store";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+registerCharts();
+
+function App() {
+  return (
+    <>
+   <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor} > 
+        <ToastContainer
+            theme="light"
+            position="bottom-left"
+            closeOnClick={true}
+            pauseOnHover={false}
+          />
+          <Navigation />
+          </PersistGate>
+    </Provider>
+    </>
+  )
+}
+
+export default App
